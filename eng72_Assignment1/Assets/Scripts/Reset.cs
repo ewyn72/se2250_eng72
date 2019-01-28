73d63bebdf1c1374278456ec84050320a0143825
Ethan Ng <ewyn101@gmail.com>
2019-01-28 13:07:51 -0500

    First commit for assignment 1

---
+++ eng72_Assignment1/Assets/Scripts/Reset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    Vector3 originalPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            gameObject.transform.position = originalPos;
+        }
+    }
+        
+    
+}
---
+++ eng72_Assignment1/Assets/Scripts/View.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour
+{
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchCamera();
+    }
+
+    void switchCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (cameraOne.activeInHierarchy)
+            {
+                cameraTwo.SetActive(true);
+                cameraOne.SetActive(false);
+            }
+            else
+            {
+                cameraOne.SetActive(true);
+                cameraTwo.SetActive(false);
+            }
+
+        }
+    }
+}
