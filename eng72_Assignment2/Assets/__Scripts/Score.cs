ce831c641d8e958c9be4cde2a8ae627b56b21615
Ethan Ng <ewyn101@gmail.com>
2019-02-11 15:48:50 -0500

    Updated nomenclature

---
+++ eng72_Assignment2/Assets/__Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score
+{
+    private int score = 0;
+    public int GetScore()
+    {
+        return score;
+    }
+    public void SetScore(int i)
+    {
+        score = i;
+    }
+
+}
