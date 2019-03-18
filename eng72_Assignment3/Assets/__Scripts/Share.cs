8ff6bec6ee477cedfd1f8293c385a5714cf8ceee
Ethan Ng <ewyn101@gmail.com>
2019-03-18 14:03:42 -0400

    Assignment 3

---
+++ eng72_Assignment3/Assets/__Scripts/Share.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Share : MonoBehaviour
+{ 
+/* Share on Twitter */
+string twitterAddress = "http://twitter.com/intent/tweet";
+    string tweetLang = "en";
+    string txt = "My highscore: ";
+    int highscore = ScoreManager.HIGH_SCORE;
+    public void shareScoreOnTwitter()
+    {
+        Application.OpenURL(twitterAddress + "?text=" + WWW.EscapeURL(txt) + highscore + "&amp:lang=" + WWW.EscapeURL(tweetLang));
+    }
+
+}
